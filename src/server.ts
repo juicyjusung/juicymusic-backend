cc702458709c6ca03e24b8378f94ded55d1d4a80
juicyjusung <juicyjusung@gmail.com>
2020-09-08 21:57:16 +0900

    chore: .env 설정

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,14 @@ import * as bodyParser from 'body-parser';
 import logger from './middleware/logger';
 
 import HomeController from './controllers/home.controller';
-
+import * as dotenv from 'dotenv';
+dotenv.config(
+  process.env.NODE_ENV === 'production'
+    ? { path: `${__dirname}/../.env.prod` }
+    : { path: `${__dirname}/../.env.dev` }
+);
 const app = new App({
-  port: 3000,
+  port: parseInt(process.env.PORT, 10),
   controllers: [new HomeController()],
   middleWares: [bodyParser.json(), bodyParser.urlencoded({ extended: true }), logger],
 });
