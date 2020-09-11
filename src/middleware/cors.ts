c6fc0f30f743ea5ff49203e74c22369302d68a01
juicyjusung <juicyjusung@gmail.com>
2020-09-12 02:58:20 +0900

    cors

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -2,7 +2,10 @@ import * as cors from 'cors';
 import { CorsOptions } from 'cors';
 
 const corsOptions: CorsOptions = {
-  // origin: "http://localhost:8080"
+  origin: 'http://localhost:8080',
+  methods: ['GET, POST, OPTIONS, PUT, DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 };
 
 const corsMiddleware = cors(corsOptions);
