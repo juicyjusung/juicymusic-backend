0c016d70e0b3c50b62650628cf9cb2097e167c64
juicyjusung <juicyjusung@gmail.com>
2020-09-15 22:30:58 +0900

    chore: CORS 환경변수 처리

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -1,8 +1,14 @@
 import * as cors from 'cors';
+import * as dotenv from 'dotenv';
 import { CorsOptions } from 'cors';
 
+dotenv.config(
+  process.env.NODE_ENV === 'production'
+    ? { path: `${__dirname}/../../.env.prod` }
+    : { path: `${__dirname}/../../.env.dev` }
+);
 const corsOptions: CorsOptions = {
-  origin: 'http://localhost:8080',
+  origin: process.env.ORIGIN,
   methods: ['GET, POST, OPTIONS, PUT, DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
